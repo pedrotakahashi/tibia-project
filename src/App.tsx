e18eb7a45e7a1214c7ba927577742b34e22e30e1
Pedro Takahashi <pedroh.takahashi@gmail.com>
2021-09-06 22:02:47 -0300

    feat(base):Created base of project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h2>Tibia Characters using API</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
